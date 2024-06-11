abe1b61020633cf9515e3108f8e2b0d207820dfe
SERTY <serty2005@gmail.com>
2024-06-11 05:37:05 +0300

    edit profitness to 0.1 add check for unknown user

--- config.py
+++ config.py
@@ -4,4 +4,5 @@ TOKENS = [
     {'name': os.getenv("HAMSTER_USER1"), "token": os.getenv("HAMSTER_TOKEN1")},
     {'name': os.getenv("HAMSTER_USER2"), "token": os.getenv("HAMSTER_TOKEN2")},
     {'name': os.getenv("HAMSTER_USER3"), "token": os.getenv("HAMSTER_TOKEN3")},
+    {'name': os.getenv("HAMSTER_USER4"), "token": os.getenv("HAMSTER_TOKEN4")},
 ]
